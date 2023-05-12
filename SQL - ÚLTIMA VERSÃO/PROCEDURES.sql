d9c09364bdc046e10e0819fd88bcfed9054d5e53
Théo Littig <theo.littig1@gmail.com>
2023-05-12 20:30:53 -0300

    Update PROCEDURES.sql

--- SQL - ÚLTIMA VERSÃO/PROCEDURES.sql
+++ SQL - ÚLTIMA VERSÃO/PROCEDURES.sql
@@ -16,3 +16,29 @@ BEGIN
         SELECT 'Email inválido' AS result
     END
 END
+
+---------------------------------------------------------------------------------------------
+
+CREATE PROCEDURE usp_fazer_reserva
+    @cpf VARCHAR(11),
+    @numero_mesa INT,
+    @data_reserva DATETIME
+AS
+BEGIN
+     IF EXISTS (SELECT 1 FROM Mesa WHERE numero_mesa = @numero_mesa AND status_mesa = 'Disponível')
+    BEGIN
+        INSERT INTO Reserva (cpf, numero_mesa, data_reserva)
+        VALUES (@cpf, @numero_mesa, @data_reserva);
+
+        UPDATE Mesa
+        SET status_mesa = 'Reservado'
+        WHERE numero_mesa = @numero_mesa;
+
+        PRINT 'Reserva realizada com sucesso!';
+
+    END
+    ELSE
+    BEGIN
+        PRINT 'A mesa selecionada não está disponível.';
+    END
+END
