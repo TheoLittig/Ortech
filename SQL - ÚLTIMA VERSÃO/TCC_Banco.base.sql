924138c7c3afe0706e40b12a0ab4fcd9f74917e5
Théo Littig <theo.littig1@gmail.com>
2023-07-07 13:40:52 -0300

    Update TCC_Banco.base.sql

--- SQL - ÚLTIMA VERSÃO/TCC_Banco.base.sql
+++ SQL - ÚLTIMA VERSÃO/TCC_Banco.base.sql
@@ -143,12 +143,12 @@ CREATE TABLE Avaliacao (
 go
 -- Criação da tabela DetalhesPedido
 CREATE TABLE DetalhesPedido (
-   id_detalhes_pedido INT IDENTITY PRIMARY KEY,
    id_pedido INT NOT NULL,
    id_produto INT NOT NULL,
    quantidade_produto INT NOT NULL,
    valor_unitario DECIMAL(10, 2) NOT NULL,
    observacoes VARCHAR(100),
+   Primary key (id_pedido, id_produto),
    FOREIGN KEY (id_pedido) REFERENCES Pedido(id_pedido),
    FOREIGN KEY (id_produto) REFERENCES Produtos(id_produto)
 );
